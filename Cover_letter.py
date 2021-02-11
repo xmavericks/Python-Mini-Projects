59dd6c0f9910d7ae0d4ae40cc0275552ac7799bb
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-11 23:57:18 +0530

    Update Cover_letter.py
    line break added

--- Cover_letter.py
+++ Cover_letter.py
@@ -63,7 +63,7 @@ def cover_letter():
     print("My name is ",name," and I am a Software Engineer with more than ",experience, " of experience as ",software_role,".","During my previous employment with",prev_org," I have worked on several technologies like", languages," and some databases experience of",databases,".\n")
     print("As I can see that you are looking for a person who has some working experience in",software_role)
     print("For that I want to say that, I might be a good fit for this role.")
-    print("\nLooking forward to hear from you !"\n")
+    print("\nLooking forward to hear from you !\n")
     print("Thanks and Regards,\n")
     print(name)
     print("Contact number: ",str(contact_number))
