ee6ceee869488dd71946e1f1be31ba29d2ee1afa
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-13 23:46:46 +0530

    Update number_pattern_55555_4444_333_decreasing.py
    updated

--- number_pattern_55555_4444_333_decreasing.py
+++ number_pattern_55555_4444_333_decreasing.py
@@ -25,3 +25,12 @@ for num in range(1,rows+1):
     item = rows+1-num  # As explained in line number 15 above, this is the same value of item which you can see gets printed after each iteration for for loop on line 14 and output line 15
     print(item,end = " ")
   print("")
+
+  
+Output for this code will be same as the above:
+    
+5 5 5 5 5 
+4 4 4 4 
+3 3 3 
+2 2 
+1
