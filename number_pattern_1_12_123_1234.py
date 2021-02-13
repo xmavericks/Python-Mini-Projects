b0aaf02aefe8eb6b4204b426e9c784302fe72608
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-13 21:02:56 +0530

    Create number_pattern_1_12_123_1234.py
    Pattern file created

---
+++ number_pattern_1_12_123_1234.py
@@ -0,0 +1,17 @@
+Printing Pattern :
+1  
+1 2  
+1 2 3  
+1 2 3 4  
+1 2 3 4 5
+
+Code: 1
+
+rows = int(input())
+
+for num in range(1,rows+1):
+  for i in range(1,num+1): # here why I iterate through 1 - (num+1) --> this simply means for(i = 1; i <= num; i++) like we use in C++/JavaScript
+    print(i, end = " ")
+  print("")
+  
+Code: 2
