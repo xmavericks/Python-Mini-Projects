c5a73b5b1a710693791961bea151ae323e3499e1
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-13 23:45:52 +0530

    Update number_pattern_55555_4444_333_decreasing.py
    updated

--- number_pattern_55555_4444_333_decreasing.py
+++ number_pattern_55555_4444_333_decreasing.py
@@ -22,6 +22,6 @@ rows = int(input())
 item = 0  # Item here is the largest value at each column in each row --->  eg 5 5 5 5 5, first row value for rows input = 5
 for num in range(1,rows+1):
   for k in range(rows+1, num, -1):
-    item = rows+1-num
+    item = rows+1-num  # As explained in line number 15 above, this is the same value of item which you can see gets printed after each iteration for for loop on line 14 and output line 15
     print(item,end = " ")
   print("")
