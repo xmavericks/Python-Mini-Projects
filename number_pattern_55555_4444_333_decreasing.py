b379922cc4f7559993e50244be09bd87cd99c32b
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-13 23:43:14 +0530

    Update number_pattern_55555_4444_333_decreasing.py
    updated

--- number_pattern_55555_4444_333_decreasing.py
+++ number_pattern_55555_4444_333_decreasing.py
@@ -14,3 +14,14 @@ for num in range(1,rows+1):
   for k in range(rows+1, num, -1):
     print(rows+1-num,end = " ") # Printing the maximum value --> explanation rows = 5, num at this iteration = 1. Max value = 5+1-1 = 5, afterwards as num increases max value will be decreases
   print("")
+
+  
+Below is another piece of code using extra variable for more clarification and visualise this clearly
+
+rows = int(input())
+item = 0  # Item here is the largest value at each column in each row --->  eg 5 5 5 5 5, first row value for rows input = 5
+for num in range(1,rows+1):
+  for k in range(rows+1, num, -1):
+    item = rows+1-num
+    print(item,end = " ")
+  print("")
