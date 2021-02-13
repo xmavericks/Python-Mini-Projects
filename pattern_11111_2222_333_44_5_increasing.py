d5b7af4aca57c69c8d08f7e877649c39796712b0
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-13 23:36:46 +0530

    Update pattern_11111_2222_333_44_5_increasing.py
    file updated

--- pattern_11111_2222_333_44_5_increasing.py
+++ pattern_11111_2222_333_44_5_increasing.py
@@ -12,6 +12,6 @@ Program code solution in Python:
 rows = int(input())
 
 for num in range(1,rows+1):
-  for k in range(rows+1, num, -1):
-    print(num,end = " ")
+  for k in range(rows+1, num, -1): # Dereasing for loop used to decrease the number of elements inside each decreasing row afterwards
+    print(num,end = " ") # Printing number, since we want it in increasing order, and num initially inside this loop has value 1, and then num ++ applied at each iteration
   print("")
