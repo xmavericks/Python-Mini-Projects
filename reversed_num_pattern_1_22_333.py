042d0bd0cc954a206f5aa99f970e1751ea35fada
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-13 22:06:00 +0530

    Update reversed_num_pattern_1_22_333.py
    updated

--- reversed_num_pattern_1_22_333.py
+++ reversed_num_pattern_1_22_333.py
@@ -1,10 +1,14 @@
 Don't get confused, from the previous pattern
 
 Previous Pattern was :
-
+        1
+      1 2  -> Line has increasing value
+    1 2 3 
+  1 2 3 4 
+1 2 3 4 5
 Current Pattern:
         1 
-      2 2 
-    3 3 3 
+      2 2 -> Line has same value, but increased by 1 from previous line, simply means print the number times number of rows - rows 2 , line 2 should be 22 with (rows-2) spaces after
+    3 3 3 -> space_count = rows-3, beacuse this is 3rd row, so we substracted 3
   4 4 4 4 
 5 5 5 5 5
