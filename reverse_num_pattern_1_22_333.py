a4ae77f4b897bddd62d25a71c93cc2acdaecd154
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-13 21:34:33 +0530

    Update reverse_num_pattern_1_22_333.py
    file created

--- reverse_num_pattern_1_22_333.py
+++ reverse_num_pattern_1_22_333.py
@@ -11,3 +11,12 @@ Solution Code:
 Program - Using Python
 
 rows = int(input())
+
+for num in range(1,rows+1):
+  for k in range(rows, num, -1): # created for loop to print extra spaces
+    print(" ",end = " ")
+  for i in range(1, num+1):  # This loop is responsible for printing values right? But we have to print spaces before the value, see line number 3 - there are 4 spaces printed before 1
+    print(i, end = " ")
+  print("")
+  
+ Note - The above program can has below cleared explanation, for watching exact number of spaces getting print before actual values
