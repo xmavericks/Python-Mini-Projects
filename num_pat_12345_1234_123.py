0fe0f0ab5a2a3ad145aa3f214f0ee36eb57b39c6
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-14 18:56:16 +0530

    Update num_pat_12345_1234_123.py
    Solution code updated

--- num_pat_12345_1234_123.py
+++ num_pat_12345_1234_123.py
@@ -7,3 +7,10 @@ Number Patter increasing - element removed at each level having maximum value
 1
 
 Solution Code:
+
+rows = int(input())
+
+for num in range(1,rows+1):
+  for k in range(rows+1, num, -1):
+    print(rows-k+2,end = " ") # This line is the reason why, number has been started gettin printed from 1
+  print("")
