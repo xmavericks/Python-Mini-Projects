6a2743c9af837605b20bdc0db70a641c1e219def
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-14 19:01:58 +0530

    Update mirror_patterns_12345_1234__1_12.py
    updated

--- mirror_patterns_12345_1234__1_12.py
+++ mirror_patterns_12345_1234__1_12.py
@@ -1,6 +1,6 @@
 Mirror Patter:
 
-1 2 3 4 5 
+1 2 3 4 5 # Consider this first half, code from line 18 - 21 is responsible for printing first half
 1 2 3 4 
 1 2 3 
 1 2 
@@ -12,3 +12,14 @@ Mirror Patter:
 1 2 3 4 5
 
 Solution Code:
+
+rows = int(input())
+
+for num in range(1,rows+1):
+    for k in range(rows+1, num, -1):
+        print(rows-k+2,end = " ")
+    print("")
+for num in range(1,rows+1):
+    for i in range(1,num+1):
+        print(i, end = " ")
+    print("")
