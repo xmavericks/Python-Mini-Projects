3c775ababeedf48e841e7702c16782f6c70bdb82
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-15 20:36:44 +0530

    Update Python-Version-Check.py
    code added

--- Python-Version-Check.py
+++ Python-Version-Check.py
@@ -1,4 +1,11 @@
-# Writing a program to print the version of Python you/me are currently using.
+### Writing a program to print the version of Python you/me are currently using.
 
 Proogram - Code
 
+import sys
+print("Python Version")
+print(sys.version)
+print("Version info.")
+print (sys.version_info)
+
+Output will be like:
