2ef09ecebe9da241c9c2d21d88dad7e8caedc639
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-15 20:44:54 +0530

    Update Current-Datetime.py
    code explanation added

--- Current-Datetime.py
+++ Current-Datetime.py
@@ -10,5 +10,5 @@ Solution Code -
 from datetime import datetime           # importing datetime module from
 print("Current Date and Time")
 print(datetime.now())                   # This line will print datetime like - 2021-02-15 15:09:35.387310
-print(datetime.date(datetime.now()))    # This line will print datetime like - 
+print(datetime.date(datetime.now()))    # This line will print datetime like - 2021-02-15 (Only date as we are calling only date from datetime)
 print(datetime.time(datetime.now()))
