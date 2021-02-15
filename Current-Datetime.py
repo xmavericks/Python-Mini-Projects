7f8ad6d4a74d8bb9f2965d036a988b09d2c28c52
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-15 20:46:56 +0530

    Update Current-Datetime.py
    EOF comment added after explanation

--- Current-Datetime.py
+++ Current-Datetime.py
@@ -3,12 +3,15 @@
 Desired Output:
 
 Current Date and Time
-2021-02-15 15:09:35.387310
+2021-02-15 15:13:58.592418
 
 Solution Code - 
 
 from datetime import datetime           # importing datetime module from
 print("Current Date and Time")
-print(datetime.now())                   # This line will print datetime like - 2021-02-15 15:09:35.387310
-print(datetime.date(datetime.now()))    # This line will print datetime like - 2021-02-15 (Only date as we are calling only date from datetime)
-print(datetime.time(datetime.now()))
+print(datetime.now())                   # This line will print datetime like - 2021-02-15 15:13:58.592418
+print(datetime.date(datetime.now()))    # This line will print date only like - 2021-02-15 (as we are calling only date from datetime)
+print(datetime.time(datetime.now()))    # This line will print time only like - 15:13:58.592487 (as we are calling only time from datetime)
+
+
+# EOF reached
