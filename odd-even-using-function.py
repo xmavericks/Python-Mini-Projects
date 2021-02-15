c61003244e9dc164be0ffffcbf7d15128fb79b24
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-16 05:07:43 +0530

    Update odd-even-using-function.py
    Output added

--- odd-even-using-function.py
+++ odd-even-using-function.py
@@ -29,5 +29,9 @@ for i in range(1, n+1): # Initial iteration range(1,n+10)
         Even(i)
         print(i, end = " ") # Space separated Even numbers
 
+        
+Output:
+Enter limit upto which you want to print numbers: 20
+1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
 
 # EOD reached
