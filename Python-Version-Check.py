a1e98be70f686f95b04079f32101ee877eac3406
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-15 20:37:28 +0530

    Update Python-Version-Check.py
    code submitted with current output

--- Python-Version-Check.py
+++ Python-Version-Check.py
@@ -9,3 +9,11 @@ print("Version info.")
 print (sys.version_info)
 
 Output will be like:
+# This
+Python Version
+3.8.7 (default, Dec 21 2020, 20:10:35) 
+[GCC 7.5.0]
+Version info.
+sys.version_info(major=3, minor=8, micro=7, releaselevel='final', serial=0)
+
+# EOF reached
