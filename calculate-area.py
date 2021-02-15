46c39854965c04446046f4303cf273a2a28b4e8a
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-15 21:11:10 +0530

    Update calculate-area.py
    code with output updated

--- calculate-area.py
+++ calculate-area.py
@@ -9,8 +9,27 @@ side1_rec, side2_rec = int(input("Enter first side value of reactangle: ")), int
 print("Given integers for multiple options for calculating areas are below: ")
 print("Radius of circle is: "+str(radius), end = "\n")
 print("Side of square is: "+str(side_square), end = "\n")
-print("Both sides of rectangle are: "+str(side1_rec)+" and "+ str(side2_rec))
+print("Both sides of rectangle are: "+str(side1_rec)+" and "+ str(side2_rec),end = "\n")
 
 print("Area of circle is == 3.141 * radius * radius -> 3.141*(radius**2) \n ===>", (radius**2)*3.141, end = "\n")
 print("Area of square is == side * side -> side_square**2\n ===> ", side_square**2, end = "\n")
 print("Area of rectangle is == length * breadth -> side1_rec * side2_rec \n ===> ", side1_rec*side2_rec)
+
+
+Output of the above code is:
+  
+Enter radius: 7
+Enter side value for square: 12
+Enter first side value of reactangle: 10
+Enter first side value of reactangle: 16
+Given integers for multiple options for calculating areas are below: 
+Radius of circle is: 7
+Side of square is: 12
+Both sides of rectangle are: 10 and 16
+
+Area of circle is == 3.141 * radius * radius -> 3.141*(radius**2) 
+ ===> 153.909
+Area of square is == side * side -> side_square**2
+ ===>  144
+Area of rectangle is == length * breadth -> side1_rec * side2_rec 
+ ===>  160
