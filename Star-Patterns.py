724a61923355a9f8f069b0ece26e616734802e91
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-16 09:05:06 +0530

    Update Star-Patterns.py
    update

--- Star-Patterns.py
+++ Star-Patterns.py
@@ -22,7 +22,7 @@ print("\nPattern 2: \n")
 #   * * * * *
 
 for num in range(1,rows+1):
-  for k in range(rows, num, -1): # created for loop to print extra spaces
+  for k in range(rows, num, -1):
     print("",end = "")
   for i in range(1, num+1):  # This loop is responsible for printing values right? But we have to print spaces before the value, see line number 3 - there are 4 spaces printed before 1
     print("*", end = " ")
