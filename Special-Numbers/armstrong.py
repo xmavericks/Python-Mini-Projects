ddf5e960111b1d16be7d08e7d750ea01c39210ef
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-17 00:06:13 +0530

    Update armstrong.py
    Armstrong definition added

--- Special-Numbers/armstrong.py
+++ Special-Numbers/armstrong.py
@@ -1,1 +1,9 @@
 # This program will tell you the exact logic behind printing all the Armstrong numbers till a range
+
+Which number is exactly said to be an Armstrong number?
+---> A number say for example 153 is said to be an Armstrong number only if sum of cube of all the digit is exactly equal to that number
+Explanation --:
+  digits in 153 are : 1, 5 and 3
+  cube of all the digits : 1*1*1 = 1, 5*5*5 = 125, 3*3*3 = 27
+  sum of  cubes of all the digits = 1 + 125 + 27 = 153 == original number
+----> This kind of numbers are said to be an Armstrong number
