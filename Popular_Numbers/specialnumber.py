cb969f8a2b46718a97c1fa9804b36022c04ec9d5
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-21 03:01:05 +0530

    Update specialnumber.py
    code added

--- Popular_Numbers/specialnumber.py
+++ Popular_Numbers/specialnumber.py
@@ -2,3 +2,40 @@
 Example - 145 is a special number
 Why?
 1! + 4! + 5! = 1 + 24 + 120 == 145 = Original number
+
+
+--------------------Program---------------------------
+
+number = int(input("Enter a number: "))
+
+def specialnumber(number):
+    copy_of_number = number
+    sumOfDigits = 0
+    # Sum of factorial of digits
+    while (copy_of_number != 0):
+       #Get the rightmost digit 
+        currentDigit = copy_of_number % 10
+        sumOfDigits = sumOfDigits + factorial(currentDigit)
+        copy_of_number = copy_of_number // 10
+        
+    #If sumOfDigits is equal to inputNumber then the number is Special, otherwise not
+    if (sumOfDigits == number):
+        return True
+    else:
+        return False
+# Function declared for calculating factorial of digits
+def factorial(n):
+    if (n == 1 or n == 0):
+        return 1
+    else:
+        return (n * factorial(n -1))
+        
+result = specialnumber(number)
+
+if (result == True):
+    print(str(number) + " is a Special number")
+else:
+    print(str(number) + " is NOT a Special number")
+    
+    
+--------------------Output---------------------------
