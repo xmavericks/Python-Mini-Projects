db36185cc8352bc33a5fdca1a4902a22a7138f1d
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-21 02:12:41 +0530

    Update checkPalindrome.py
    code updated

--- Special-Numbers/checkPalindrome.py
+++ Special-Numbers/checkPalindrome.py
@@ -1,13 +1,37 @@
-num=int(input("Enter a number:"))
-temp=num
-def checkPalindrome(num):
-    rev=0
-    while(num>0):
-        dig=num%10
-        rev=rev*10+dig
-        num=num//10
-    if(temp==rev):
-        print("The number is palindrome!")
+# Any number is said to be a Palindrome, if and only if all the digits are same from both ends. 
+Example - 121 == 121, 123 != 321
+
+---------------------------Program 1 : Taking input as a String-------------------------------
+
+number = int(input("Enter number to check if it is a palindrome or not: "))
+copy_of_number = number
+def checkPalindrome(number):
+    reverse = 0
+    while(number>0):
+        digit = number % 10
+        reverse = (reverse * 10) + digit
+        number = number // 10
+    if(copy_of_number == reverse):
+        print("Palindrome")
     else:
         print("Not a palindrome!")
-checkPalindrome(num)
+checkPalindrome(number)
+
+---------------------------Program 2 : Taking input as a String--------------------------------
+    
+n, result, comparator= input("Enter number to check if it is a palindrome or not: "),[], []
+result.append(n[::-1])
+comparator.append(n[0:])
+def checkPalindrome(n):
+    flag = False
+    if result == comparator:
+        flag = True
+    if flag == True:
+        print("Palindrome")
+    else:
+        print("Not a Palindrome")
+checkPalindrome(n)
+
+----------------------Output----------------------
+Enter number to check if it is a palindrome or not: 12345678987654321
+Palindrome
