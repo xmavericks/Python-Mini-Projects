c5cecb1134cfbdf9bed794fe2fecdc89daca5a7d
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-21 02:49:06 +0530

    Create Special-Number.py
    created

---
+++ Popular Numbers/Special-Number.py
@@ -0,0 +1,4 @@
+# A number is said to be a Special Number only if sum of factorial of all the digits are same as the original number.
+
+Example - 145 is a Special number, here is why?
+1! + 4! + 5! = 1 + 24 + 120 = 145 = original number
