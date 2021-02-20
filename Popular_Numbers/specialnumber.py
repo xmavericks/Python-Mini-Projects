9e14f0712f2cc71763541f770715100b6382e374
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-21 02:58:37 +0530

    Create specialnumber.py
    file created

---
+++ Popular_Numbers/specialnumber.py
@@ -0,0 +1,4 @@
+# special number are number whose sum of factorials of all the digits are equal to the original number.
+Example - 145 is a special number
+Why?
+1! + 4! + 5! = 1 + 24 + 120 == 145 = Original number
