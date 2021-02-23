04642024ca0a155df5ee5f832e0bd42585153a90
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-24 02:31:23 +0530

    Create GeeksForGeeks_Special_Pattern.py
    File created

---
+++ GeeksForGeeks_Special_Pattern.py
@@ -0,0 +1,17 @@
+# We are about to print pattern like this below:
+
+G  
+G e  
+G e e  
+G e e k  
+G e e k s  
+G e e k s F  
+G e e k s F o  
+G e e k s F o r  
+G e e k s F o r G  
+G e e k s F o r G e  
+G e e k s F o r G e e  
+G e e k s F o r G e e k  
+G e e k s F o r G e e k s
+
+# For this problem you need to think the logic on your on, and I must say that you can not find solution to this pattern anywhere on any website including GeeksForGeeks as well.
