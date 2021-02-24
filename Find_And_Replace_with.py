ca46ca984f8bd064b10a685817976c5c98fc5f5e
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-24 07:49:56 +0530

    file created and example added

---
+++ Find_And_Replace_with.py
@@ -0,0 +1,7 @@
+# This is a program where you will get to learn behind the scene of find and replace with.
+Say for example, we have shortcut key ctrl + H in Notepad++ where we can replace any word or character, with new word.
+
+Example - 
+
+phrase - '''Hi my name is Amod Kumar, I came from Software Engineering Background. I always wanted to be a Scientist but end up becoming an Engineer'''
+say, we want to replace name Amod with new name - Mahesh and the entire phrase will be changed for Amod Kumar to for Mahesh Kumar.
