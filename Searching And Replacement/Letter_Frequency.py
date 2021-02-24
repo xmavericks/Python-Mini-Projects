2667c14681fff7336f08efcdeedc5323471cd1de
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-25 01:09:23 +0530

    file updated

--- Searching And Replacement/Letter_Frequency.py
+++ Searching And Replacement/Letter_Frequency.py
@@ -12,3 +12,27 @@ Sample Output:
 40
 
 The letter l appears 2 times in the text hello, which has 5 letters. So, the frequency would be (2/5)*100 = 40
+
+
+-------------------------- Program : ----------------------------------------------
+
+# This function is responsible for calculating frequency
+def frequency(appearance, size):
+    return int((appearance/size)*100)
+
+# This function is counting the occurence of searching element in original string
+def countOccurence(searchingChar, originalString):
+    count = 0
+    for i in originalString:
+        if i == searchingChar:
+            count = count + 1
+    return count
+
+# Asking input of originalString
+originalString = input()
+# Asking input for searchingChar inside the original string
+searchingChar = input()
+# occurence Storing in count variable
+count = countOccurence(searchingChar, originalString)
+# Printing the output
+print(frequency(count, len(originalString)))
