9736e8ebac72eea879ea741105ec67c21b866813
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-24 11:22:05 +0530

    code added

--- Searching And Replacement/counting_vowels.py
+++ Searching And Replacement/counting_vowels.py
@@ -11,3 +11,25 @@ this is great
 
 Sample Output:
 4
+
+
+------------------------------Program---------------------------------------
+
+#your code goes here
+
+st = input()
+
+count = 0
+
+for x in st:
+    if x == 'a':
+        count += 1
+    elif x == 'e':
+        count += 1
+    elif x == 'i':
+        count += 1
+    elif x == 'o':
+        count += 1
+    elif x == 'u':
+        count += 1
+print(count)
