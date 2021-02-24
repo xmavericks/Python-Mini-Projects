29b87279966191f26ad162615c79aaf717c7cd34
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-24 07:54:17 +0530

    phrase variable created

--- Searching And Replacement/Find_And_Replace_with.py
+++ Searching And Replacement/Find_And_Replace_with.py
@@ -5,3 +5,8 @@ Example -
 
 phrase - '''Hi my name is Amod Kumar, I came from Software Engineering Background. I always wanted to be a Scientist but end up becoming an Engineer'''
 say, we want to replace name Amod with new name - Mahesh and the entire phrase will be changed for Amod Kumar to for Mahesh Kumar.
+
+
+------------------------Program--------------------------
+# First line take the input for the original Phrase
+phrase = input("Please input some paragraph as your initial phrase, say for example you can tell me something about yourself? : ")
