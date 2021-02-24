38b3c807a2934992996ac7172cb4f724be572a79
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-24 08:09:46 +0530

    initial condition added

--- Searching And Replacement/Find_And_Replace_with.py
+++ Searching And Replacement/Find_And_Replace_with.py
@@ -10,11 +10,15 @@ say, we want to replace name Amod with new name - Mahesh and the entire phrase w
 ------------------------Program--------------------------
 # First line take the input for the original Phrase
 phrase = input("Please input some paragraph as your initial phrase, say for example you can tell me something about yourself? : ")
-print("Is this phrase correct according to you? : \n",phrase)
+print("Do you want to modify anything in your phrase? : \n",phrase)
+boolean = input("Yes/No : ")  # This boolean will ask if he wants to modify anything
 
-# This boolean will ask if he wants to modify anything
-boolean = input("Yes/No : ")
-
+# Conditional Statement added here to ask users if they want to change anything
+if boolean = "Yes" or "yes" or "YES":
+  OldWord = input("What would like to change in your earlier phrase? : ")
+  print("You want to replace this - ", OldWord)
+  NewWord = input("Enter replacing word here : ")
+  
 
 # This function will look for word user want to change
 def replacingWord(OldWord):
