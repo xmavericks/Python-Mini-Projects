3a18d97423246cbdf72438a5016d6e83e4b42ec4
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-24 22:04:59 +0530

    code updated in C++

--- GeeksForGeeks_Special_Pattern.py
+++ GeeksForGeeks_Special_Pattern.py
@@ -17,3 +17,39 @@ G e e k s F o r G e e k s
 # For this problem you need to think the logic on your on, and I must say that you can not find solution to this pattern anywhere on any website including GeeksForGeeks as well.
 
 I will update the solution for this problem tomorrow.
+
+---------------------Program: C++ ----------------------------------
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    cout << "Type your name: ";
+    cin >> name;
+    cout << "Your name is: " << name;
+
+    for(int i = 0; i < name.length()+1; i++){
+        for(int j = 1; j < i+1; j++){
+            cout<< name[j-1]<< " ";
+        }
+        cout << "" << endl;
+    }
+    return 0;
+}
+
+-----------------------Output : C++ -------------------------------
+Type your name: GEEKSFORGEEKS
+Your name is: GEEKSFORGEEKS
+G 
+G E 
+G E E 
+G E E K 
+G E E K S 
+G E E K S F 
+G E E K S F O 
+G E E K S F O R 
+G E E K S F O R G 
+G E E K S F O R G E 
+G E E K S F O R G E E 
+G E E K S F O R G E E K 
+G E E K S F O R G E E K S
