56531a626ca5216486921a07d472b9e744bb5fe8
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-26 00:56:40 +0530

    file created

---
+++ Working_with_Dictionary/list_dict_tuple.py
@@ -0,0 +1,24 @@
+------------------- Program --------------------
+
+def main():
+    # list
+    
+    list = ["one", "two"]
+    
+    # dictionary 
+    
+    dict = {1:"one", 2:"two"}
+    
+    # tuple
+    
+    tp = ("one", "two")
+    
+    print("This is list: ", list)
+    print("This is a dictionary: ", dict)
+    print("This is a Tuple: ", tp)
+
+if __name__ == "__main__":
+    main();
+
+
+----------------- Output -------------------------
