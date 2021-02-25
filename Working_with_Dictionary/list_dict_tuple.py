46da47878672090d1f93aa6b279d23db140fdb91
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-26 00:59:29 +0530

    code updated with output

--- Working_with_Dictionary/list_dict_tuple.py
+++ Working_with_Dictionary/list_dict_tuple.py
@@ -2,19 +2,16 @@
 
 def main():
     # list
-    
     list = ["one", "two"]
     
     # dictionary 
-    
     dict = {1:"one", 2:"two"}
     
     # tuple
-    
     tp = ("one", "two")
     
-    print("This is list: ", list)
-    print("This is a dictionary: ", dict)
+    print("This is list: ", list,"\n")
+    print("This is a dictionary: ", dict,"\n")
     print("This is a Tuple: ", tp)
 
 if __name__ == "__main__":
@@ -22,3 +19,8 @@ if __name__ == "__main__":
 
 
 ----------------- Output -------------------------
+This is list:  ['one', 'two']
+    
+This is a dictionary:  {1: 'one', 2: 'two'}
+    
+This is a Tuple:  ('one', 'two')
