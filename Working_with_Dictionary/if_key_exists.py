c86fb316bc1cf8f22fbd48194a401a91770999d5
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-26 00:47:47 +0530

    file modified

--- Working_with_Dictionary/if_key_exists.py
+++ Working_with_Dictionary/if_key_exists.py
@@ -21,3 +21,24 @@ if __name__ == "__main__":
 True
 False
 True
+
+
+Dictionaries
+
+A useful dictionary function is get(). It does the same thing as indexing, but if the key is not found in the dictionary it returns another specified value instead.
+
+Example:
+
+def main():
+  pairs = {1: "apple",
+    "orange": [2, 3, 4], 
+    True: False, 
+    12: "True",
+  }
+
+  print(pairs.get("orange"))
+  print(pairs.get(7, 42))
+  print(pairs.get(12345, "not found"))
+
+if __name__ == "__main__":
+  main();
