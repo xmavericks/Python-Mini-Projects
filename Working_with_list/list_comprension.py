2edd5d6a200dafb906e9ac232b98e2b7f026fec3
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-26 00:15:47 +0530

    code added

--- Working_with_list/list_comprension.py
+++ Working_with_list/list_comprension.py
@@ -1,1 +1,19 @@
 # This file contains a basic example to print all the numbers from 0-100 which is multiple of both 3 and 5 using list comprehension
+
+
+------------------ Program: Python -----------------------------
+
+def main():
+    upperLimit = int(input("Enter Upper limit: "))
+    def multipleof_3_and_5(upperLimit):
+        res = [x for x in range(100) if x%3== 0 if x%5==0]
+        
+        for i in res:
+            print(i, end=" ")
+    
+    multipleof_3_and_5(upperLimit)
+
+if __name__ == "__main__":
+    main();
+
+--------------------Output: -----------------------------------
