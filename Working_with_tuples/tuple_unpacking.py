92fe3ec16e004c3aeafa0335a28b0f9eca377d97
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-26 11:02:37 +0530

    example added

---
+++ Working_with_tuples/tuple_unpacking.py
@@ -0,0 +1,12 @@
+# Tuple Unpaccking
+
+Tuple Unpacking ~
+Tuple unpacking allows you to assign each item in a collection to a variable.
+
+Example:
+
+numbers = (1, 2, 3, 4)
+a,b,c,d = numbers
+print("a is : ",a, "\nb is : ",b, "\nc is : ", c, "\nd is : ", d)
+
+Output:
