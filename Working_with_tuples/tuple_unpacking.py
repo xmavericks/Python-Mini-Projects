371a04ce120e2594a27b15bd52b3d295cf02ac4d
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-26 11:17:46 +0530

    changes made

--- Working_with_tuples/tuple_unpacking.py
+++ Working_with_tuples/tuple_unpacking.py
@@ -15,3 +15,21 @@ a is :  1
 b is :  2 
 c is :  3 
 d is :  4
+
+
+Tuple Unpacking
+A variable that is prefaced with an asterisk (*) takes all values from the collection that are left over from the other variables.
+
+Example : 
+
+a, b, *c, d = (1, 2, 3, 4, 5, 6, 7, 8, 9) or [1, 2, 3, 4, 5, 6, 7, 8, 9]  -- we can initiate using either square bracket or parenthesis
+print(a)
+print(b)
+print(c)
+print(d)
+
+___ Output ___
+a is :  1
+b is :  2
+*c Holds all values till last item :  [3, 4, 5, 6, 7, 8]
+d is :  9
