83ea1e5ba59dbe67a79e98d160ee1e82f7c774e6
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-26 10:43:53 +0530

    re-assign error

--- Working_with_tuples/tuple.py
+++ Working_with_tuples/tuple.py
@@ -11,3 +11,11 @@ words = ("spam", "eggs", "sausages")
 You can access the values in the tuple with their index, just as you did with lists:
  
 print(words[0]) // gives output as :: spam
+
+
+Re-assigning value:
+
+Trying to reassign a value in a tuple causes an error.
+words[0] = "maggi"
+
+Error message - 
