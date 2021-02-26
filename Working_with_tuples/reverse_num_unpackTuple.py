df44018cd1c6239bcb53cb1446596db66a2c2cbb
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-26 11:12:12 +0530

    example added

--- Working_with_tuples/reverse_num_unpackTuple.py
+++ Working_with_tuples/reverse_num_unpackTuple.py
@@ -4,4 +4,12 @@ numbers = (1, 2)
 a,b = numbers
 print("a is : ",a, "\nb is : ",b)
 a, b, sum = b, a, a+b
-print("a is : ",a, "\nb is : ",b, "\nSum of a and b is : ", sum)
+print("a is now : ",a, "\nb is now : ",b, "\nSum of a and b is : ", sum)
+
+___ Output ____
+
+a is :  1 
+b is :  2
+a is now :  2 
+b is now :  1 
+Sum of a and b is :  3
