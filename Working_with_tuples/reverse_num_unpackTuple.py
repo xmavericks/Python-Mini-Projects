d8a940877d441bc6d39ebd769a2b59ed82d52644
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-26 11:10:52 +0530

    updated

--- Working_with_tuples/reverse_num_unpackTuple.py
+++ Working_with_tuples/reverse_num_unpackTuple.py
@@ -1,2 +1,7 @@
 # Reversing two number using tuple unpacking and also find sum of the number
 
+numbers = (1, 2)
+a,b = numbers
+print("a is : ",a, "\nb is : ",b)
+a, b, sum = b, a, a+b
+print("a is : ",a, "\nb is : ",b, "\nSum of a and b is : ", sum)
