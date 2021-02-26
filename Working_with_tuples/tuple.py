4d082107847840572150fe2baaefc4d5e4dc62be
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-02-26 10:42:41 +0530

    added definition

--- Working_with_tuples/tuple.py
+++ Working_with_tuples/tuple.py
@@ -1,1 +1,13 @@
 # Definition File
+
+Tuples~
+
+Tuples are very similar to lists, except that they are immutable (they cannot be changed).
+Also, they are created using parentheses, rather than square brackets.
+
+Example:
+words = ("spam", "eggs", "sausages")
+
+You can access the values in the tuple with their index, just as you did with lists:
+ 
+print(words[0]) // gives output as :: spam
