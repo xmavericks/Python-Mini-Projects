e69b6e2d04873bb581a432496f3b63ac5389d8b6
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-03-16 22:36:08 +0530

    bubble sort code added

---
+++ Sorting_Algorithms/Bubble_Sort.py
@@ -0,0 +1,22 @@
+# This program is added for bubble sort.
+
+# For very large value of length of Array -> It will fail
+# Try to check the constraint, if n = lenght of array > 1000, it will give Time Limit Exceeded
+
+Program : 1 -  without using third variable for swapping elements
+  
+arr = [1, 5, 3, 2]
+
+def bubble_sort(arr):
+    for i in range(n-1,0,-1):
+        for j in range(i):
+            if arr[j] > arr[j+1]:
+            arr[j] = arr[j] + arr[j+1]
+            arr[j+1] = arr[j] - arr[j+1]
+            arr[j] = arr[j] - arr[j+1]
+    return arr
+print(bubble_sort(arr))
+
+Output:
+    
+1 2 3 5
