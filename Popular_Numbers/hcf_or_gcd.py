dd4c1fda596f7b32d7aa38b729699c85b19d8f7c
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-03-16 19:32:37 +0530

    file created and code added

---
+++ Popular_Numbers/hcf_or_gcd.py
@@ -0,0 +1,44 @@
+# This file contains the method to calculate GCD/HCF of two numbers
+
+Method 1: Using recursive approach 1
+ 
+from fractions import gcd
+def main():
+  a , b = int(input()), int(input())
+	def gcd(a,b):
+	    if a == 0:
+	    	return b
+	    return gcd(b % a, a)
+	print(gcd(a,b))
+
+if __name__ == "__main__":
+	main();
+  
+  
+Method 2: Using recursive approach 2
+
+# Recursive function to return gcd of a and b
+def gcd(a,b):
+     
+    # Everything divides 0
+    if (a == 0):
+        return b
+    if (b == 0):
+        return a
+ 
+    # base case
+    if (a == b):
+        return a
+ 
+    # a is greater
+    if (a > b):
+        return gcd(a-b, b)
+    return gcd(a, b-a)
+ 
+# Driver program to test above function
+a = int(input())
+b = int(input())
+if(gcd(a, b)):
+    print('GCD of', a, 'and', b, 'is', gcd(a, b))
+else:
+    print('not found')
