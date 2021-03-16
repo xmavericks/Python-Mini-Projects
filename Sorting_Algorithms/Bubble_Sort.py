c4aad7416077f9fd3562fbdd22c2e0f5e97ae146
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-03-16 22:39:04 +0530

    code updated

--- Sorting_Algorithms/Bubble_Sort.py
+++ Sorting_Algorithms/Bubble_Sort.py
@@ -20,3 +20,22 @@ print(bubble_sort(arr))
 Output:
     
 1 2 3 5
+
+
+Program : 2 - using third variable for swapping elements
+  
+arr = [1, 5, 3, 2]
+
+def bubble_sort(arr):
+    for i in range(n-1,0,-1):
+        for j in range(i):
+            if arr[j] > arr[j+1]:
+            temp = arr[j]
+            arr[j] = arr[j+1]
+            arr[j+1] = temp
+    return arr
+print(bubble_sort(arr))
+
+Output:
+    
+1 2 3 5
