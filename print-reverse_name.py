182a0c7646d9bf115cc18747151b471c1d53397b
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-03-16 19:51:42 +0530

    updated

--- print-reverse_name.py
+++ print-reverse_name.py
@@ -10,14 +10,8 @@ Code solution: Solution 1 using 2 for loops for -> example amod kumar --> kumar 
   
 first_name = input("Enter your first name: ") # Taking input of first name
 last_name = input("Enter your last name: ")   # Taking input of last name
-f_name_reverse = []                           # Declaring list variable to store reversed names respectively
-l_name_reverse = []
 first_name_reversed = ""                      # Declaration of empty string variable fo storing each char from reversed name and append it invertly
 last_name_reversed = ""
-for i in range(len(first_name)):
-    f_name_reverse.append(first_name[i])      # Performing reversing of letter
-for j in range(len(last_name)):
-    l_name_reverse.append(last_name[j])
 
 for k in range(len(f_name_reverse)):          # storing in empty string
     first_name_reversed += f_name_reverse[k]
