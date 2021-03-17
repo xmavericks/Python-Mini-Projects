fdbfbbdf965326b12f93eb3aefb2f39e61999eed
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-03-17 16:38:29 +0530

    update

--- Sorting_Algorithms/Bubble_Sort.py
+++ Sorting_Algorithms/Bubble_Sort.py
@@ -11,9 +11,9 @@ def bubble_sort(arr):
     for i in range(n-1,0,-1):
         for j in range(i):
             if arr[j] > arr[j+1]:
-            arr[j] = arr[j] + arr[j+1]
-            arr[j+1] = arr[j] - arr[j+1]
-            arr[j] = arr[j] - arr[j+1]
+              arr[j] = arr[j] + arr[j+1]
+              arr[j+1] = arr[j] - arr[j+1]
+              arr[j] = arr[j] - arr[j+1]
     return arr
 print(bubble_sort(arr))
 
