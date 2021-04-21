452d03e18e9a000a78e61c75d16e0876c408d8e2
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-04-21 12:39:57 +0530

    code push

--- Threading/odd-even-independent-thread.py
+++ Threading/odd-even-independent-thread.py
@@ -1,1 +1,3 @@
 # This file is created for giving one example to print odd even number till a specific range using 2 independent threads
+
+Below code is written for printing odd and even number independently using two different threads
