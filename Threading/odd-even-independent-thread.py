f36f4b8ae8c4f797b00e49bb1f22b1475f04d559
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-04-21 12:47:33 +0530

    updated

--- Threading/odd-even-independent-thread.py
+++ Threading/odd-even-independent-thread.py
@@ -39,5 +39,5 @@ Output :
 
 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
 
-Explantion - 1,3,5,7 till 19 printed using first thread
+Explantion - 1,3,5,7 and so on... printed using first thread
              2,4,6,8 and so on... printed using second thread
