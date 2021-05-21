ebb717dc9066f61a3e764afb98467d4240f2fdec
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-05-21 22:08:38 +0530

    updated

--- Sorting_Algorithms/Insertion_sort.py
+++ Sorting_Algorithms/Insertion_sort.py
@@ -3,7 +3,6 @@ def insertion_sort(InputList):
 	for i in range(1, len(InputList)):
 		j = i-1
 		nxt_element = InputList[i]	# Comparing the current element with next one
-
 		while (InputList[j] > nxt_element) and (j >= 0):
 			InputList[j+1] = InputList[j]
 			j=j-1
