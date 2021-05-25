eb4a002e07e9ff651984750d268851d7bfddee6d
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-05-26 00:18:54 +0530

    updated

--- Binary Search/Program_1.py
+++ Binary Search/Program_1.py
@@ -1,5 +1,24 @@
 def main():
-	
+	arr = [2, 1, 4, 5, 3, 8, 7, 6, 10, 9, 11]
+	arr.sort()
+	n = len(arr)-1
+	low, high = 0, n
+	print(arr)
+
+	def search(arr, low, high, x):
+		if low <= high:
+			mid = low + (high-low) // 2
+			if arr[mid] < x:
+				low = mid + 1
+				return search(arr, low, high, x)
+			elif arr[mid] > x:
+				high = mid - 1
+				return search(arr, low, high, x)
+			else:
+				print("Element is found at: ", mid)
+		else:
+			print("Element is not present in the given array: ", arr)
+	search(arr,low, high,8)
 
 if __name__ == "__main__":
 	main();
@@ -7,6 +26,5 @@ if __name__ == "__main__":
 Output :
 
 searching element = 8
-
 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 Element is found at:  7
