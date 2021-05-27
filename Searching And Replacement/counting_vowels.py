f832d022d6bbb3a41c2f07b16b566f6bbd4a10a1
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-05-28 00:36:02 +0530

    updated

--- Searching And Replacement/counting_vowels.py
+++ Searching And Replacement/counting_vowels.py
@@ -1,9 +1,7 @@
 Accessing Strings
 
-
 You need to make a program that counts the number of vowels in a given text.
 The vowels are a, e, i, o, and u.
-
 Take a string as input and output the number of vowels.
 
 Sample Input:
@@ -12,15 +10,12 @@ this is great
 Sample Output:
 4
 
-
 ------------------------------Program---------------------------------------
 
 #your code goes here
 
 st = input()
-
 count = 0
-
 for x in st:
     if x == 'a':
         count += 1
