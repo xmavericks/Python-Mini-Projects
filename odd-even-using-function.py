d66fe81936a9bd185cd4bc1a9965705bc60e6bdf
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-06-15 01:36:14 +0530

    updated

--- odd-even-using-function.py
+++ odd-even-using-function.py
@@ -5,7 +5,7 @@ output should be - 1 2 3 4 5 6 7 8 9 10
 
 
 Soultion Code:
-  
+
 n = int(input("Enter limit upto which you want to print numbers: "))
 counter = 1
 
@@ -33,7 +33,6 @@ for i in range(1, n+1): # Initial iteration range(1,n+10)
 Output:
 Enter limit upto which you want to print numbers: 20
 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
-
 
 
 Some of you might think, that this can be achieved only using 3 lines of code. I agree, but please read the very first line again and then saw 3 liner slotution below:
