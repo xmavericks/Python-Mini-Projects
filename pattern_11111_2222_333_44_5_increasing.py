dff121f1e04f0fe99a372851c9158762f18a6324
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-06-16 00:03:40 +0530

    updated

--- pattern_11111_2222_333_44_5_increasing.py
+++ pattern_11111_2222_333_44_5_increasing.py
@@ -15,3 +15,5 @@ for num in range(1,rows+1):
   for k in range(rows+1, num, -1): # Dereasing for loop used to decrease the number of elements inside each decreasing row afterwards
     print(num,end = " ") # Printing number, since we want it in increasing order, and num initially inside this loop has value 1, and then num ++ applied at each iteration
   print("")
+
+ #EOF Reached
