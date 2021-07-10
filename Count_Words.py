4b045955d5d5280dcb882e88b376b6fe5bb40cb0
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-07-11 02:16:48 +0530

    updated

--- Count_Words.py
+++ Count_Words.py
@@ -8,3 +8,17 @@ print("Total number of words entered by you are: "+str(count))
 Output - 
 Enter any sentence want: leetcode is awesome # Entered this sentence
 Total number of words entered by you are: 3
+
+#Another Method
+
+words = "Hi there this is me, Amod"
+count = 1
+
+for i in range(len(words)):
+	if words[i] == " ":
+		count += 1
+
+print(count)
+
+Output:
+6
