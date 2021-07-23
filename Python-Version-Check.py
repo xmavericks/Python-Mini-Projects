651c0773c619522e29eaa247e37c124ec55ba2cd
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-07-24 00:56:59 +0530

    updated

--- Python-Version-Check.py
+++ Python-Version-Check.py
@@ -1,4 +1,4 @@
-### Writing a program to print the version of Python you/me are currently using.
+### Writing a program to print the version of Python you/me are currently using
 Proogram - Code
 
 import sys
