11c342b92a6b31c2bded3a0f7e78ad00cd721a85
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-08-14 21:58:43 +0530

    updated

--- Working_with_tuples/tuple.py
+++ Working_with_tuples/tuple.py
@@ -1,5 +1,5 @@
 # Definition File
-Tuples~
+Tuples
 
 Tuples are very similar to lists, except that they are immutable (they cannot be changed).
 Also, they are created using parentheses, rather than square brackets.
