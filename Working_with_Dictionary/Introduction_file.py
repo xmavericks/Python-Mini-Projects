b88ddd78d7319d5a887d79ffb721c25fa17e0078
Amod Kumar <37140777+xmavericks@users.noreply.github.com>
2021-08-22 00:03:25 +0530

    updated

--- Working_with_Dictionary/Introduction_file.py
+++ Working_with_Dictionary/Introduction_file.py
@@ -3,7 +3,7 @@ We saw how lists allow to store elements with their corresponding indices.
 The indices in a list are automatically set. But what if we need to set our own index?
 
 Dictionaries are another collection type and allow us to map arbitrary keys to values.
-Dictionaries can be indexed in the same way as lists, using square brackets containing keys.
+Dictionaries can be indexed in the same way as lists, using square brackets containing keys
 
 Example:-
 
